95bf20786d0b04f2b2edb2215f48be6cd0a790cb
Vitalii Chyhryn <chyhryn.vitalii@gmail.com>
2025-06-04 05:02:57 +0300

    Add new endpoint

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
-
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrices')
+def matrix() -> dict:
+    matrix_a = np.random.randint(low=0, high=128, size=(10 * 10))
+    matrix_b = np.random.randint(low=0, high=128, size=(10 * 10))
+    product = np.dot(matrix_a, matrix_b)
+    return {"matrix_a": matrix_a, "matrix_b": matrix_b, "product": product}
